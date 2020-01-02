df5753ee3805d3dda88bd6d902a08ef61ee59c3b
aloryde <1994676-aloryde@users.noreply.gitlab.com>
2020-01-02 14:37:56 -0700

    Nothing

--- CoffeeDemo/demo/src/main/java/com/alora/demo/DemoApplication.java
+++ CoffeeDemo/demo/src/main/java/com/alora/demo/DemoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
+	
 	@Autowired
 	MenuService menuService;
 

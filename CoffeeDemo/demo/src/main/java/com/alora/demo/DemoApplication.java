b5d89342629d1c7a31f5a39c4edcb0c1485280cc
aloryde <1994676-aloryde@users.noreply.gitlab.com>
2020-01-02 14:29:43 -0700

    pulled docker file from online repo.

--- CoffeeDemo/demo/src/main/java/com/alora/demo/DemoApplication.java
+++ CoffeeDemo/demo/src/main/java/com/alora/demo/DemoApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
-
 	@Autowired
 	MenuService menuService;
 
